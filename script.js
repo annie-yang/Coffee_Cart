86900d9932b8387a480eac70a4a91ccb920c272d
Annie Y <ayang5@csustan.edu>
2017-06-11 10:33:16 -0700

    edited prices

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.orderForm.onclick = function(){
 	var size = document.orderForm.group1.value;
 	var cream = document.orderForm.group2.value;
+	var sugar = document.orderForm.group3.value;
+	var ice = document.orderForm.group4.value;
 	var whipcream = document.orderForm.group5.value;
 	var style = document.orderForm.group6.value;
 
-	var totalPrice = +size + +cream + +whipcream + +style;
+	var totalPrice = +size + +cream + +sugar + +ice + +whipcream + +style;
 	price.innerHTML = "Price: " + totalPrice;
 }
